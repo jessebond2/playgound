fa34492bb949d7f0b09f2362896d498b9a156a9c
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2022-06-10 17:11:20 -0700

    adding progress

--- giants-foundry/bin/fetchItems.ts
+++ giants-foundry/bin/fetchItems.ts
@@ -1,6 +1,6 @@
 import { Item } from "runescape-api/lib/RuneScape";
 import { grandexchange } from "runescape-api/osrs";
-import { createWriteStream } from "fs";
+import { createWriteStream, createReadStream } from "fs";
 import { join } from "path";
 import asyncBatch from "async-batch";
 
@@ -9,14 +9,31 @@ async function fetchResult(id: number) {
     const result = await grandexchange.getItem(id);
     return result;
   } catch (e) {
-    console.log(e);
+    // console.log("fetchResults", e);
     return undefined;
   }
 }
 
-const LIMIT = 1000;
+const LIMIT = 30000;
 const CONCURRENCY_LIMIT = 10;
-const IDS = [Array.from(Array(LIMIT).keys())];
+const IDS = Array.from(Array(LIMIT).keys());
+
+async function loadItemIds() {
+  const path = join(__dirname, "..", "data", "raw item ids.txt");
+  const readable = createReadStream(path, { encoding: "utf8" });
+
+  const ids = [];
+  for await (const line of readable) {
+    const parts = line.split(" ");
+    if (parts[2] !== "null") {
+      ids.push(parseInt(parts[0]));
+    }
+  }
+
+  console.log(`Loaded ${ids.length} ids`);
+
+  return ids;
+}
 
 async function main() {
   const writeStream = createWriteStream(
@@ -24,34 +41,37 @@ async function main() {
   );
 
   const result: Record<number, Item> = {};
-  // for (let i = 0; i < LIMIT; i++) {
-  //   const progress = ((0 / LIMIT) * 100).toFixed(2);
-  //   const res = await fetchResult(i);
-  //   if (res) {
-  //     result[i] = res;
-  //     console.log(`itemId ${i} found. ${progress}% done`);
-  //   } else {
-  //     console.log(`itemId ${i} not found. ${progress}% done`);
-  //   }
-  // }
+  const loadedIds = await loadItemIds();
+
   let progress = 0;
   const asyncResult = await asyncBatch(
-    IDS,
-    async (task, taskIndex) => {
-      const res = await fetchResult(taskIndex);
+    // IDS,
+    loadedIds,
+    async (task) => {
+      const res = await fetchResult(task);
       progress++;
+      const completion = ((progress / LIMIT) * 100).toFixed(2);
       if (res) {
-        result[taskIndex] = res;
-        console.log(`itemId ${taskIndex} found. ${progress}% done`);
+        result[task] = res;
+        console.log(`itemId ${task} found. ${completion}% done`);
       } else {
-        console.log(`itemId ${taskIndex} not found. ${progress}% done`);
+        console.log(`itemId ${task} not found. ${completion}% done`);
       }
     },
     CONCURRENCY_LIMIT
   );
 
-  console.log("Writing result");
-  writeStream.write(JSON.stringify(result));
+  const sortedItems = Object.keys(result)
+    .sort()
+    .reduce((obj, key) => {
+      obj[parseInt(key)] = result[parseInt(key)];
+      return obj;
+    }, {} as Record<number, Item>);
+  console.log(
+    `Finished looking up items. Found ${Object.keys(result).length} items`
+  );
+
+  writeStream.write(JSON.stringify(sortedItems));
   writeStream.on("finish", () => {
     console.log("Finished writing file");
     writeStream.end();
